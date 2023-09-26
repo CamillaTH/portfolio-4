e31f7864abd0ecfc20502d9a20b4df4e04e2496f
CamillaTH <hiltulacamilla@gmail.com>
2023-09-26 14:37:47 +0000

    added comment model

--- portfolio4/models.py
+++ portfolio4/models.py
@@ -1,3 +1,6 @@
 from django.db import models
-
+from django.conf import settings
 # Create your models here.
+class Comment(models.Model):
+    author = models.ForeignKey(settings.AUTH_USER_MODEL,on_delete=models.CASCADE,)
+    comment = models.CharField(max_length=350, null=False, blank=False)
