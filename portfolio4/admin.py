593cdf8b5adb0e7e8d2f00aa72eff091740a7278
CamillaTH <hiltulacamilla@gmail.com>
2023-09-26 15:15:06 +0000

    added commentlike and post models

--- portfolio4/admin.py
+++ portfolio4/admin.py
@@ -1,4 +1,8 @@
 from django.contrib import admin
 from .models import Comment
+from .models import CommentLike
+from .models import Post
 # Register your models here.
 admin.site.register(Comment)
+admin.site.register(CommentLike)
+admin.site.register(Post)
--- portfolio4/models.py
+++ portfolio4/models.py
@@ -1,6 +1,31 @@
 from django.db import models
 from django.conf import settings
+
 # Create your models here.
+
+#Model that stores the comments on a post
 class Comment(models.Model):
     author = models.ForeignKey(settings.AUTH_USER_MODEL,on_delete=models.CASCADE,)
     comment = models.CharField(max_length=350, null=False, blank=False)
+
+    def __str__(self):
+        return str(self.author)
+
+#Model that stores the likes om a comment
+class CommentLike(models.Model):
+     author = models.ForeignKey(settings.AUTH_USER_MODEL,on_delete=models.CASCADE,)
+     comment = models.ForeignKey(Comment,on_delete=models.CASCADE,)
+     likes = models.IntegerField()
+     
+     def __str__(self):
+        return str(self.author)
+
+#Model that stores the post
+class Post(models.Model):
+    author = models.ForeignKey(settings.AUTH_USER_MODEL,on_delete=models.CASCADE,)
+    comment = models.ForeignKey(Comment,on_delete=models.CASCADE,)
+    post = models.CharField(max_length=900, null=False, blank=False)
+
+    def __str__(self):
+        return str(self.author)
+
