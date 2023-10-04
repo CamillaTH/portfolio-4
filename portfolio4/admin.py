d792a8cb8298abfe27be5ec2e42734ba966e6ab2
CamillaTH <hiltulacamilla@gmail.com>
2023-10-04 21:46:33 +0000

    added category model

--- portfolio4/admin.py
+++ portfolio4/admin.py
@@ -1,8 +1,7 @@
 from django.contrib import admin
-from .models import Comment
-#from .models import CommentLike
-from .models import Post
+from .models import Comment, Post, Category
 # Register your models here.
 admin.site.register(Comment)
 admin.site.register(Post)
+admin.site.register(Category)
 #admin.site.register(CommentLike)
--- portfolio4/models.py
+++ portfolio4/models.py
@@ -6,9 +6,20 @@ from django.utils import timezone
 import datetime
 
 # Create your models here.
+class Category(models.Model):
+    '''Model that stores categories'''
+    name = models.CharField(max_length=40)
+    slug = models.SlugField()
+    image = CloudinaryField('image', default='placeholder')
 
-#Model that stores the post
+    class Meta:
+        ordering =['name']
+
+    def __str___(self):
+        return self.name
+
 class Post(models.Model):
+    '''Model that stores the post'''
     author = models.ForeignKey(settings.AUTH_USER_MODEL,on_delete=models.CASCADE, related_name='author_posts')
     #comment = models.ForeignKey(Comment,on_delete=models.CASCADE,null=True, blank=True)
     heading = models.CharField(max_length=150, unique=True)
@@ -18,6 +29,7 @@ class Post(models.Model):
     slug = models.SlugField(max_length=200, unique=True)
     image = CloudinaryField('image', default='placeholder')
     likes = models.ManyToManyField(User, related_name='post_likes', blank=True)
+    categories = models.ManyToManyField(Category)
 
     class Meta:
         ordering =['-creationTime']
@@ -28,26 +40,18 @@ class Post(models.Model):
     def amount_of_likes(self):
         return self.likes.count()
 
-#Model that stores the comments on a post
 class Comment(models.Model):
+    '''Model that stores the comments on a post'''
     post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name="comments")
     #author = models.ForeignKey(settings.AUTH_USER_MODEL,on_delete=models.CASCADE, related_name='author_comment')
     content = models.TextField(blank=True)
     creationTime = models.DateTimeField(auto_now_add=True)
     likes = models.ManyToManyField(User, related_name='comment_likes', blank=True)
     image = CloudinaryField('image', default='placeholder')
+    slug = models.SlugField()
 
     class Meta:
         ordering = ['creationTime']
 
     def __str__(self):
         return f"Comment {self.content} by "#{self.author.first_name} {self.author.last_name}"
-
-#Model that stores the likes on a comment
-#class CommentLike(models.Model):
-#     author = models.ForeignKey(settings.AUTH_USER_MODEL,on_delete=models.CASCADE,)
-#     comment = models.ForeignKey(Comment,on_delete=models.CASCADE,)
-#     likes = models.IntegerField()
-#     
-#     def __str__(self):
-#        return str(self.author)
