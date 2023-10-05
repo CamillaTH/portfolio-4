4c8d86ae48fc7529b7edc09aade92fc7dc56def8
CamillaTH <hiltulacamilla@gmail.com>
2023-10-05 19:06:56 +0000

    added summernote field to post content field and prepopulated slug field, AND prepopulated category slugfield

--- portfolio4/admin.py
+++ portfolio4/admin.py
@@ -5,8 +5,13 @@ from django_summernote.admin import SummernoteModelAdmin
 @admin.register(Post)
 class PostAdmin(SummernoteModelAdmin):
 
+    prepopulated_fields = {'slug': ('heading',)}
     summernote_field = ('content')
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    prepopulated_fields = {'slug': ('name',)}
+
 
 # Register your models here.
 admin.site.register(Comment)
-admin.site.register(Category)
