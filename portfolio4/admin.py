aabf07dff8e28f7a47443010ea3299e7dadb0971
CamillaTH <hiltulacamilla@gmail.com>
2023-10-05 19:41:34 +0000

    added searchFields and list_displays to post and comment

--- portfolio4/admin.py
+++ portfolio4/admin.py
@@ -7,12 +7,17 @@ class PostAdmin(SummernoteModelAdmin):
 
     prepopulated_fields = {'slug': ('heading',)}
     summernote_field = ('content')
+    list_display = ('heading', 'slug', 'approved', 'creation_time')
     list_filter = ('approved', 'creation_time')
+    search_fields = ('categories', 'heading')
 
 @admin.register(Category)
 class CategoryAdmin(admin.ModelAdmin):
+
     prepopulated_fields = {'slug': ('name',)}
     list_filter = ('name',)
+    search_fields = ('name',)
+    list_display = ('name', 'slug')
 
 
 # Register your models here.
