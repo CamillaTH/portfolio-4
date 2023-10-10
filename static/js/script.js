c8c78b72256d7f1ea1cc06420f8c19a1fc8b0bd6
CamillaTH <hiltulacamilla@gmail.com>
2023-10-10 12:03:01 +0000

    added animatiations to like buttons

--- static/js/script.js
+++ static/js/script.js
@@ -1,9 +1,17 @@
 $(function(){
+
+/*---------------------------------------
+  Constants
+-----------------------------------------*/
+//const timeout3Seconds = setTimeout(3000);
+
+
+
 /*---------------------------------------
   Functions
 -----------------------------------------*/
 
-    //Script that animates the counts of likes 
+    //function that animates the counts of likes 
     $.initLikeAnimation = function () {
         $('#like-counter').each(function () {
         $(this).prop('like-counter',0).animate({
@@ -16,24 +24,46 @@ $(function(){
             }
         });
     });
-}
+    }
+
+    // removes shaker class 
+    $.removeShakerClass = function() {
+        $("#like-post-detail,#like-comment-detail").removeClass( "fa-shake" );
+    }
+
+
+function stopTimeout3Seconds() {
+    clearTimeout(timeout3Secionds);
+  }
 
 /*---------------------------------------
   Animations and style manipulations  
 -----------------------------------------*/
     //add tootltip to like container
     $(".like-container").attr('title', 'View post to like it!');
+    
+    //add tootltip to like container detail
+    $(".like-container-detail,#like-comment-detail").attr('title', 'Like post!');
+
+    $("#like-post-detail,#like-comment-detail").hover(
+        function () {
+         console.log("sadas")
+          $(this).addClass("fa-solid");
+        },
+        function () {
+            $(this).removeClass("fa-solid");
+          }
+    )
+
+    // add shake animation to like button
+    $("#like-post-detail,#like-comment-detail").on( "click", function() {
+      $("#like-post-detail,#like-comment-detail").addClass( "fa-shake" );
+      //wait 2 seconds then remove shaker class
+      timeout = setTimeout($.removeShakerClass, 2000);
+    });
 
     //init like animation
     $.initLikeAnimation();
 
-
-
-
-
-
-
-
-    
 
   });
