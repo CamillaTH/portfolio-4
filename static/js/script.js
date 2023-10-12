ba9edfe2ddb96e40e98f49d553e15a5492fb66e9
CamillaTH <hiltulacamilla@gmail.com>
2023-10-12 20:09:37 +0000

    added js logic to show message after comment form is submmited plus blink animation

--- static/js/script.js
+++ static/js/script.js
@@ -26,15 +26,40 @@ $(function(){
     });
     }
 
-    // removes shaker class 
-    $.removeShakerClass = function() {
-        $("#like-post-detail,#like-comment-detail").removeClass( "fa-shake" );
+    //show comment submittet msg
+    $(document).ready(function() {
+      const storedCommentMessage = localStorage.getItem('commentConfirmationMessage');
+      if (storedCommentMessage) {
+        $('.comment-confirmation').show().text(storedCommentMessage);
+        // Clear the stored message
+        localStorage.removeItem('commentConfirmationMessage');
+        //wait 4 seconds then hide comment comfirmation msg
+        timeout = setTimeout($.hideCommentConfirmationMsg, 4000);
+       }
+
+      $('.comment-form > button').bind('click', function(){
+        $.showCommentConfirmationMsg();
+      });
+    });
+
+    // hides comment confirmation message 
+    $.hideCommentConfirmationMsg = function() {
+      $('.comment-confirmation').hide();
+      //remove confirmation from message local storage
+      localStorage.removeItem('commentConfirmationMessage');
     }
 
-
-function stopTimeout3Seconds() {
-    clearTimeout(timeout3Secionds);
-  }
+    // hides comment confirmation message 
+    $.showCommentConfirmationMsg = function() {
+      $('.comment-confirmation').show();
+      //add confirmation message to local storage since when form is submitted the page realoads 
+      localStorage.setItem('commentConfirmationMessage', 'Your comment have been submitted! It will be shown when approved by post owner');
+    }
+    
+     // removes shaker class 
+     $.removeShakerClass = function() {
+      $("#like-post-detail,#like-comment-detail").removeClass( "fa-shake" );
+    }
 
 /*---------------------------------------
   Animations and style manipulations  
@@ -58,11 +83,11 @@ function stopTimeout3Seconds() {
     $("#like-post-detail,#like-comment-detail").on( "click", function() {
       $("#like-post-detail,#like-comment-detail").addClass( "fa-shake" );
       //wait 2 seconds then remove shaker class
-      timeout = setTimeout($.removeShakerClass, 2000);
+      timeout = setTimeout($.removeShakerClass, 5000);
     });
 
+  
     //init like animation
     $.initLikeAnimation();
-
 
   });
