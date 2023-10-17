bf4e47e1f4a5db7a275a818fb2dbe43e05c4c0fc
CamillaTH <hiltulacamilla@gmail.com>
2023-10-17 11:42:30 +0000

    uninstall whitenoise

--- protfolio4_django/wsgi.py
+++ protfolio4_django/wsgi.py
@@ -10,11 +10,7 @@ https://docs.djangoproject.com/en/3.2/howto/deployment/wsgi/
 import os
 
 from django.core.wsgi import get_wsgi_application
-#from whitenoise.django import DjangoWhiteNoise
-#from dj_static import Cling
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'protfolio4_django.settings')
 
 application = get_wsgi_application()
-#application = DjangoWhiteNoise(application)
-#application = Cling(get_wsgi_application())
