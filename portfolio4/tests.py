1c4a0b7c393b73ba1a469e05c1ca0d4847ed2985
CamillaTH <hiltulacamilla@gmail.com>
2023-10-17 12:23:16 +0000

    added test for like post

--- portfolio4/tests.py
+++ portfolio4/tests.py
@@ -1,3 +1,28 @@
 from django.test import TestCase
+from django.urls import reverse
+from django.contrib.auth.models import User
+from portfolio4.models import Post
 
-# Create your tests here.
+class LikePostViewTest(TestCase):
+    def setUp(self):
+        # Create a user and a post
+        self.user = User.objects.create_user(username='testuser', password='password123')
+        self.post = Post.objects.create(
+            author=self.user,
+            heading='Test Post',
+            content='Test content',
+            slug='test-post'
+        )
+
+    def test_like_post_authenticated_user(self):
+        # Log in the user
+        self.client.login(username='testuser', password='password123')
+
+        # Send a POST request to like the post
+        response = self.client.post(reverse('like_post', kwargs={'slug': self.post.slug}))
+
+        # Check that the response is a redirect
+        self.assertEqual(response.status_code, 302)
+
+        # Check that the post is liked by the user
+        self.assertTrue(self.post.likes.filter(id=self.user.id).exists())
